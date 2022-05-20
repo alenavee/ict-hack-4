5b31a00ccb396f60820cd5a98aa03d5114f41553
Matvei Borisov <bme15122000@gmail.com>
2022-05-21 00:27:23 +0300

    init ios app project

---
+++ ict-hack-4/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ict-hack-4
+//
+//  Created by Матвей Борисов on 20.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		view.backgroundColor = .purple
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
