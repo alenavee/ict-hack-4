cc8ae42c9ef2efee5f71403d820cca0b81338c90
Matvei Borisov <bme15122000@gmail.com>
2022-05-21 14:50:15 +0300

    create main tab bar screen

---
+++ ict-hack-4/MainViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MainViewController.swift
+//  ict-hack-4
+//
+//  Created by Матвей Борисов on 21.05.2022.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+	
+	init() {
+		super.init(nibName: nil, bundle: nil)
+		configureViewControllers()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		tabBar.backgroundColor = .white
+	}
+	
+	private func configureViewControllers() {
+		let first = UIViewController()
+		first.view.backgroundColor = .yellow
+		let tabOne = UINavigationController(rootViewController: first)
+		let tabOneBarItem = UITabBarItem(title: "Новая запись", image: UIImage(named: AppImageNames.create), tag: 0)
+		tabOne.tabBarItem = tabOneBarItem
+		
+		let second = UIViewController()
+		second.view.backgroundColor = .green
+		let tabTwo = UINavigationController(rootViewController: second)
+		let tabTwoBarItem = UITabBarItem(title: "История", image: UIImage(named: AppImageNames.history), tag: 1)
+		tabTwo.tabBarItem = tabTwoBarItem
+		
+		viewControllers = [tabOne, tabTwo]
+	}
+
+
+}
