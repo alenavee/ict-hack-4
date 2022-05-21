538597a6ed31646998becc375b336ffffb6b4c55
Matvei Borisov <bme15122000@gmail.com>
2022-05-21 04:04:28 +0300

    add new colors and icons

---
+++ ict-hack-4/Resources/AppColors.swift
@@ -0,0 +1,18 @@
+//
+//  AppColors.swift
+//  ict-hack-4
+//
+//  Created by Матвей Борисов on 21.05.2022.
+//
+
+import UIKit
+
+struct AppColors {
+	static let primaryColor = UIColor(red: 172/255, green: 121/255, blue: 255/255, alpha: 1)
+	
+	static let borderPrimaryColor = UIColor(red: 204/255, green: 172/255, blue: 255/255, alpha: 1)
+	
+	static let textPrimary = UIColor(red: 60/255, green: 60/255, blue: 60/255, alpha: 1)
+	
+	static let mainBackground = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1)
+}
---
+++ ict-hack-4/Resources/AppImageNames.swift
@@ -0,0 +1,16 @@
+//
+//  AppImageNames.swift
+//  ict-hack-4
+//
+//  Created by Матвей Борисов on 21.05.2022.
+//
+
+struct AppImageNames {
+	static let touchId = "touch-id"
+	
+	static let faceId = "face-id"
+	
+	static let clear = "clear"
+	
+	static let lock = "lock"
+}
